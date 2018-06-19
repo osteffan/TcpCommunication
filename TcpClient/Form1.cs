25ba60bf427a64460fe022201f1f4708b5520b17
Olaf Steffan <olaf.steffan@weischer.net>
2018-06-19 08:36:12 +0200

    Initial commit

---
+++ TcpClient/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TcpClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                var client = new System.Net.Sockets.TcpClient("localhost", 3434);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                var message = "Hallo Message";
+                var data = Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                var stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                this.textBox1.Text += $@"Sent: {message}";
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new byte[256];
+
+                // String to store the response ASCII representation.
+
+                // Read the first batch of the TcpServer response bytes.
+                var bytes = stream.Read(data, 0, data.Length);
+                var responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                this.textBox1.Text += $@"Received: {responseData}";
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException ex)
+            {
+                this.textBox1.Text += $@"ArgumentNullException: {ex}";
+            }
+            catch (SocketException ex)
+            {
+                this.textBox1.Text += $@"SocketException: {ex}";
+            }
+        }
+    }
+}
---
+++ TcpServer/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Der Server...
+namespace TcpServer
+{
+    public partial class Form1 : Form
+    {
+        private bool serverRun = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMessageAsync(string message)
+        {
+            Invoke(new Action(() =>
+            {
+                var lineText = $"{DateTime.Now:s}: {message}\r\n";
+                TextBoxOutput(this.textBox1, lineText);
+            }));
+        }
+
+        private static void TextBoxOutput(TextBoxBase textbox, string output)
+        {
+            textbox.AppendText(output);
+            textbox.SelectionStart = textbox.Text.Length;
+            textbox.SelectionLength = 0;
+            textbox.ScrollToCaret();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                TcpListener server = null;
+                try
+                {
+                    server = new TcpListener(IPAddress.Parse("127.0.0.1"), 3434);
+                    server.Start();
+                    var bytes = new byte[256];
+
+                    // Enter the listening loop.
+                    while (this.serverRun)
+                    {
+                        ShowMessageAsync(@"Waiting for a connection... ");
+
+                        // Perform a blocking call to accept requests.
+                        // You could also user server.AcceptSocket() here.
+                        var client = server.AcceptTcpClient();
+                        ShowMessageAsync(@"Connected!");
+
+                        // Get a stream object for reading and writing
+                        var stream = client.GetStream();
+
+                        int i;
+
+                        // Loop to receive all the data sent by the client.
+                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            // Translate data bytes to a ASCII string.
+                            var data = Encoding.ASCII.GetString(bytes, 0, i);
+                            ShowMessageAsync($@"Received: {data}");
+
+                            // Process the data sent by the client.
+                            data = data.ToUpper();
+
+                            var msg = Encoding.ASCII.GetBytes(data);
+
+                            // Send back a response.
+                            stream.Write(msg, 0, msg.Length);
+                            ShowMessageAsync($@"Sent: {data}");
+                        }
+
+                        // Shutdown and end connection
+                        client.Close();
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    ShowMessageAsync($@"SocketException: {ex.Message}");
+                }
+                finally
+                {
+                    // Stop listening for new clients.
+                    server?.Stop();
+                }
+            });
+
+
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.serverRun = false;
+        }
+    }
+}
